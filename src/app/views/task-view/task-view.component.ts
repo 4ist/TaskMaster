2b35a26b1e8b16be04fff57afd86bbbadf1428f9
Mark Todd <marktodd.hl@gmail.com>
2020-05-29 18:30:00 +0200

    Add expanding to task view

--- src/app/views/task-view/task-view.component.ts
+++ src/app/views/task-view/task-view.component.ts
@@ -1,41 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Task } from "../../models/task";
-import { Status } from "../../models/task";
+import {
+    Component,
+    Input,
+} from '@angular/core';
+
+import {
+    Status,
+    Task,
+} from '../../models/task';
 
 @Component({
   selector: 'app-task-view',
   templateUrl: './task-view.component.html',
-  styleUrls: ['./task-view.component.css']
+  styleUrls: ['./task-view.component.css'],
 })
-export class TaskViewComponent implements OnInit {
+export class TaskViewComponent {
+  public expanded = false;
 
   @Input() task: Task;
 
   unicodeStatuses = {
-    [Status.Incomplete]: "\u2610",
-    [Status.Complete]: "\u2611"
-  }
+    [Status.Incomplete]: '\u2610',
+    [Status.Complete]: '\u2611',
+  };
 
-  constructor() {}
-
-  ngOnInit(): void {
-  }
-
-  GetUnicodeStatus(task: Task): string{
+  getUnicodeStatus(task: Task): string {
     return this.unicodeStatuses[task.status];
   }
 
-  ToggleStatus(task: Task){
-    if (task.status == Status.Incomplete)
-      task.status = Status.Complete;
-
-    else if(task.status == Status.Complete)
-      task.status = Status.Incomplete;
+  onToggleStatus() {
+    if (this.task.status === Status.Incomplete) {
+      this.task.status = Status.Complete;
+    } else if (this.task.status === Status.Complete) {
+      this.task.status = Status.Incomplete;
+    }
   }
 
-  OpenDetails(event: Event){
-    console.log(event);
+  onToggleDetails() {
+    this.expanded = !this.expanded;
   }
-
-
 }
